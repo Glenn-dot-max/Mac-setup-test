2db98f524349c3260b0827bcd97e7e0073922a5c
Glenn Duval <glenn.duval14cd@gmail.com>
2025-11-03 20:06:58 -0800

    Add console.js - My first commit from Macbook Air M4

---
+++ console.js
@@ -0,0 +1,11 @@
+console.log("🎉 Mon Mac est connecté à GitHub !");
+
+const setup = {
+  computer: "MacBook Air M4",
+  ram: "24GB",
+  storage: "512GB",
+  connected: true,
+  github: "Glenn-dot-max",
+};
+
+console.log(setup);
